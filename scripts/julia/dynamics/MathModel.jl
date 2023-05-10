532a1fb736ff42faab3c464848cb2d0a21800d8b
Mauricio Ribeiro <61286097+ScienceMau@users.noreply.github.com>
2023-05-10 10:56:07 -0300

    Update MathModel.jl

--- scripts/julia/dynamics/MathModel.jl
+++ scripts/julia/dynamics/MathModel.jl
@@ -3,44 +3,33 @@
 ######################################################
 @inline @inbounds function MathModel(u,par,t)
 
-
-
 	######################
 	## parameters
 	#####################
+    a     = 1.6;
+    omega = 1.0;
+    r     = 0.1;
+    b     = 0.05;
+    c     = 0.35;
+    e    = 0.0001;
+    p     = par[1];
+    f0    = 0.2;
+    d     = 4/27;
+    beta  = 0.05;
 
-zeta   = 0.1;
-eta   = 0.05;
-theta = 0.35;
-a     = 1.6;
-a1    = 4.0/27.0;
-b     = par[1];
-c     = 0.00001;
-omega = 1.0;
-f0    = 0.2;
-	
-	
-	######################
-	## Equation array u[]
-	####################
 
 	du1 = u[2];
-	du2 = -zeta*u[1]-eta*u[1]-theta*u[1]-a1/(a+u[1])^2-(b*u[2]/(a+u[1])^3)+c/(a+u[1])^8+f0*cos(omega*t);
-
-
-	################
-	## Return function 
-	#################
+ 	du2 = -r*u[2]-b*u[1]-c*u[1]^3+(e/((a+u[1])^8))-(d/((a+u[1])^2))-(p*u[2]/((a+u[1])^3))+beta/((a+u[1])^4)+f0*cos(omega*t);
+	
 	return SVector{2}(du1, du2);
 end
 
 
-	as = range(0.0,0.0001, length = 300)
+	as = range(0.0,0.2, length = 300)
 	u0 = [0.0, 0.0]
-	TIME1 =  1500;
+	TIME1 =  15000;
 
 	X1=Any[];
 	X2=Any[];
 	Lyap = SharedMatrix{Float64}(length(as),length(u0));
 	T01 = SharedArray{Float64}(length(as));
-
